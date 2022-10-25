f99737b549125869f3e623ec4c122e08b4fa3a5e
mariotey <tey.mingchuan@yahoo.com.sg>
2022-10-26 01:03:00 +0800

    Added location changing feature

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import Flask, Response, render_template, request
 
 app = Flask(__name__)
 
+#################################################################################################
+
 @app.route("/")
 def main_page():
     return render_template("main.html")
+
+#################################################################################################
 
 @app.route("/Game")
 def start_page():
@@ -19,6 +23,25 @@ def instruction_page():
 # def edit_page():
 #    return render_template("edit.html")
 
+#################################################################################################
+
+@app.route("/VoidDeck")
+def voiddeck_page():
+    return "VoidDeck"
+
+@app.route("/WetMarket")
+def wetmarket_page():
+    return "WetMarket"
+
+@app.route("/Park")
+def park_page():
+    return "Park"
+
+@app.route("/HawkerCentre")
+def hawkercentre_page():
+    return "HawkerCentre"
+
+#################################################################################################
 if __name__ == "__main__":
     app.run(debug=True)
 
